924cb009c60973005185c3f3d4a8371021f4b55f
AnaïsCD <anaisc100@gmail.com>
2026-02-17 12:23:00 +0100

    ex1 finished

--- module08/ex1/ft_operator.py
+++ module08/ex1/ft_operator.py
@@ -1,4 +1,5 @@
 import sys
+import importlib
 
 
 def main():
@@ -6,24 +7,32 @@ def main():
     if sys.prefix == sys.base_prefix:
         print("You are not in a VE")
     else:
+        dependencies_dic = {"pandas": "Data manipulation",
+                            "numpy": "for pandas",
+                            "requests": "Network acess ready",
+                            "matplotlib": "Visualisation ready"}
         try:
-            import pandas
-            import requests
-            import matplotlib
-            print(f"[OK] {pandas.__name__} ({pandas.__version__}) "
-                  "- Data manipulation ready")
-            print(f"[OK] {requests.__name__} ({requests.__version__})- "
-                  "Data manipulation ready")
-            print(f"[OK] {matplotlib.__name__} ({matplotlib.__version__})- "
-                  "Data manipulation ready")
+            for name, desc in dependencies_dic.items():
+                depend = importlib.import_module(name)
+                print(f"[OK] {depend.__name__} ({depend.__version__}) "
+                      f"- {desc}")
+        except ModuleNotFoundError:
+            print(f"use either requirements or poetry to install {name}")
+        else:
+            import numpy as np
+            import matplotlib.pyplot as plt
             print("\nAnalyzing Matrix data...\n"
                   "Processing 1000 data points...\n"
                   "Generating visualization...")
-
+            data = np.random.randn(1000)
+            plt.figure(figsize=(10, 6))
+            plt.plot(data, color='green')
+            plt.title("Matrix Data Stream Analysis")
+            plt.xlabel("Random x values")
+            plt.ylabel("Random y values")
+            plt.savefig('analysis.png')
             print("\nAnalysis complete!")
             print("Results saved to: matrix/analysis.png")
-        except ModuleNotFoundError:
-            print("use either requirements or toml to install dependencies")
 
 
 if __name__ == "__main__":
