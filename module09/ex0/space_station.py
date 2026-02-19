7bc9b6c5c1b97bbfe0ce89d1081f20c3805b1a28
AnaïsCD <anaisc100@gmail.com>
2026-02-19 09:02:41 +0100

    nn

--- module09/ex0/space_station.py
+++ module09/ex0/space_station.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError
 
 
 class SpaceStation(BaseModel):
@@ -12,23 +12,54 @@ class SpaceStation(BaseModel):
     power_level: float = Field(..., ge=0.0, le=100.0)
     oxygen_level: float = Field(..., ge=0.0, le=100.0)
     last_maintenance: datetime
-    is_operational: bool = Field(..., default=True)
+    is_operational: bool = Field(default=True)
     notes: str = Field(..., max_length=200)
 
 
 def main():
     """ Testing the validation class"""
     print("Space Station Data Validation")
-    print("========================================")
+    print("========================================\n")
     valid_data = {
         "station_id": "ISS001",
         "name": "International Space Station",
         "crew_size": 6,
         "power_level": 85.5,
-        "oxygen_level": 92.3
+        "oxygen_level": 92.3,
+        "last_maintenance": "2026-02-18T14:30:00Z",
+        "notes": "this is the test "
     }
-    valid = SpaceStation(**valid_data)
-    print(f"the data is: {valid.items}")
+    try:
+        valid = SpaceStation(**valid_data)
+        print(f"ID: {valid.station_id}")
+        print(f"Name: {valid.name}")
+        print(f"Crew: {valid.crew_size}")
+        print(f"Power: {valid.power_level}%")
+        print(f"Oxygen: {valid.oxygen_level}%")
+        if valid.is_operational is True:
+            print("Status: Operational")
+        else:
+            print("Status: Not operational")
+    except ValidationError as e:
+        for error_message in e.errors():
+            print(error_message["msg"])
+
+    print("========================================\n")
+    print("Expected validation error:")
+    invalid_data = {
+        "station_id": "ISS001",
+        "name": "International Space Station",
+        "crew_size": 25,
+        "power_level": 85.5,
+        "oxygen_level": 92.3,
+        "last_maintenance": "2026-02-18T14:30:00Z",
+        "notes": "this is the test "
+    }
+    try:
+        invalid_data = SpaceStation(**invalid_data)
+    except ValidationError as e:
+        for error_message in e.errors():
+            print(error_message["msg"])
 
 
 if __name__ == "__main__":
